d5734115d629ea389608385f4bede39e7eea27b7
Emma Ågren <agren.gastronomi@hotmail.com>
2026-02-12 19:42:26 +0100

    Skriver ut tabellen

--- vite projekt, lab2/src/main.js
+++ vite projekt, lab2/src/main.js
@@ -11,7 +11,7 @@ async function fetchCourses() {
   try {
     const response = await fetch("https://webbutveckling.miun.se/files/ramschema.json");
 
-    if [!response.ok] {
+    if (!response.ok) {
       throw new Error("Fel vid hämtning av data");
     }
 
@@ -23,3 +23,22 @@ async function fetchCourses() {
     console.error("Något gick fel:", error);
   }
 }
+
+//Skriv ut tabell
+function renderTable(data) {
+  const tableBody = document.getElementById("courseTable");
+  tableBody.innerHTML = "";
+
+  data.forEach(course => {
+    const row = document.createElement("tr");
+
+     row.innerHTML = `
+     <td>${course.code}</td>
+     <td>${course.coursename}</td>
+     <td>${course.progression}</td>
+     `;
+
+     tableBody.appendChild(row);
+  });
+}
+
